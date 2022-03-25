ae3c86813bc75388425162e8bfac8dcc9610682a
mollikacomputer <mollikacomputer3@gmail.com>
2022-03-25 21:04:22 +0600

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2> Lucky One assignment 8</h2>
-      <h3> Test with Ranjit K Mandal for code upload private repo</h3>
+      <Header></Header>
+      
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2> Demented Buyer </h2>
+            <h3> Choose 4 Car </h3>
+        </div>
+    );
+};
+
+export default Header;
